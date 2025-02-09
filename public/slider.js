0c23e6f9f8ce10e56052dd9ad9a7d6f9b98b4672
Nathan Deziel <nathan.deziel@polymtl.ca>
2025-02-08 23:03:38 -0500

    data

--- public/slider.js
+++ public/slider.js
@@ -2,5 +2,6 @@ var slider = document.getElementById("myRange");
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-    document.getElementById('year').innerText = Math.floor(this.value / 10 + 2015);
+    const year = Math.floor(this.value / 10 + 2015);
+    document.getElementById('year').innerText = year;
 } 
